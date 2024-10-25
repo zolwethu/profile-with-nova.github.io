918bd30162687ffec66aad7ce28182488544d6cc
zolwethu <zithasbu621@gmail.com>
2024-10-25 11:13:57 +0200

    Nebula- message animation, loading circles animation

--- Nova/bot.js
+++ Nova/bot.js
@@ -1,21 +1,30 @@
 const chatBody = document.querySelector(".chat-body");
 const textInput = document.querySelector("#text-input");
 const send = document.querySelector(".send");
+const loadingEle = document.querySelector(".loading");
 
 send.addEventListener("click", () => renderUserMessage());
 
-// press enter to send message
+// KEY PRESS ENTER TO SEND MESSAGE
 textInput.addEventListener("keyup", (event) => {
     if (event.keyCode === 13) {
         renderUserMessage();
     }
 });
 
+// RENDER USER MESSAGES
 const renderUserMessage = () => {
     const userInput = textInput.value;
-    renderMessageEle(userInput);
+    renderMessageEle(userInput, "user");
     textInput.value = "";
-    renderChatbotResponse(userInput);
+
+    toggleLoading(false);
+    // DELAY CHATBOT RESPONSES WITH SETTIMEOUT
+    setTimeout(() => {
+        renderChatbotResponse(userInput);
+        setScrollPosition();
+        toggleLoading(true);
+    }, 1200);
 };
 
 // RENDER CHATBOT RESPONSES 
@@ -24,10 +33,15 @@ const renderChatbotResponse = (userInput) =>{
     renderMessageEle(res);
 };
 
-const renderMessageEle = (txt) => {
+const renderMessageEle = (txt, type) => {
+    // DIFF CHAT BORDER STYLES FOR USER AND BOT
+    let className = "user-message";
+    if (type !== "user"){
+        className = "chatbot-message";
+    }
     const messageEle = document.createElement("div");
     const textNode = document.createTextNode(txt);
-    messageEle.classList.add("user-message");
+    messageEle.classList.add(className);
     messageEle.append(textNode);
     chatBody.append(messageEle);
 };
@@ -36,3 +50,13 @@ const renderMessageEle = (txt) => {
 const getChatbotResponse = (userInput) => {
     return responseObj[userInput] == undefined ? "Please try something else" : responseObj[userInput];
 };
+
+// SET SCROLL POSITION
+const setScrollPosition = () =>{
+    if(chatBody.scrollHeight > 0){
+        chatBody.scrollTop = chatBody.scrollHeight;
+    }
+}
+
+// HIDE N SHOW LOADING ANIMATION
+const toggleLoading = (show) => loadingEle.classList.toggle("hide", show);
